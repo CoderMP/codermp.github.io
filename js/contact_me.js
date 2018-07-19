977f4bc5783f3c668984b4488f539b55fe3b569e
mark philips <mphilips15@gmail.com>
2018-07-19 16:57:05 -0400

    Revised script for mail sending.

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,21 +1,23 @@
 $(function() {
-    $("#submit").click(function() {
-        var data = {
-            name: $("#name").val(),
-            email: $("#email").val(),
-            subject: $("#subject").val(),
-            message: $("#message").val()
-        };
+    $("#submit").on('click touchstart', function(e) {
+        e.preventDefault();
+        $("#submit").html("<i class='fa fa-cog fa-spin'></i> Sending").prop('disabled', true);
+        var $email_form = $("#emailForm");
+        var emailData = $email_form.serialize();
 
         $.ajax({
             type: "POST",
-            url: "mail.php",
-            data: data,
+            url: "php/mail.php",
+            data: emailData,
             success: function(){
-                $("#response").addClass("text-success visible").removeClass("invisible")
+                if (response.status){
+                    $("#emailForm .input").val('');
+                    $("#emailForm .textarea").val('');
+                    $("#response").addClass("text-success visible").removeClass("invisible").text("Email Sent!");
+                }
             },
             error: function(){
-                $("#response").addClass("text-danger visible").removeClass("invisible")
+                $("#response").addClass("text-danger visible").removeClass("invisible").text("Email Failed to send!");
             }
         });
 
